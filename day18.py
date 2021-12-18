47d0d33f7bf500a79ed6b480625fc9fbbce13120
James Mayr <jamesbmayr@gmail.com>
2021-12-18 17:48:14 -0500

    day18.py

---
+++ day18.py
@@ -0,0 +1,197 @@
+# data
+
+data = ['[[8,8],5]','[[[[9,0],1],4],[[3,6],[0,5]]]','[[9,[0,[4,5]]],[1,[[6,8],4]]]','[[8,7],[[[8,5],[2,0]],[[6,3],[5,0]]]]','[[[1,8],2],[[[9,1],[2,0]],[1,[9,4]]]]','[[[6,[8,8]],[6,4]],[[8,2],[[0,8],9]]]','[[[6,3],[9,[9,1]]],[[0,0],1]]','[[[[2,7],[8,2]],[[9,6],[5,1]]],[[[7,6],[6,0]],[4,2]]]','[[[8,[9,1]],[9,3]],[[[5,4],[8,0]],[[3,5],[9,5]]]]','[[[3,[4,9]],2],[[7,9],7]]','[[[7,[9,0]],5],[[[3,4],[2,6]],[[3,5],[7,2]]]]','[[8,[8,9]],[[[3,2],[6,2]],4]]','[[[[8,0],3],[3,8]],[[[5,0],[7,3]],[5,[3,0]]]]','[4,[[3,[0,9]],[[5,0],[2,0]]]]','[[[[0,1],5],[3,[9,6]]],[[[4,4],5],[[3,8],[5,1]]]]','[[[[4,8],8],0],[5,[[1,7],[4,3]]]]','[[3,[[1,1],[5,6]]],[7,[[4,0],[0,7]]]]','[9,[4,[[1,3],2]]]','[[[1,[2,7]],[[4,7],3]],[2,1]]','[[[9,5],[2,5]],[[[8,9],[4,5]],2]]','[[2,[[7,4],6]],[[1,[0,7]],[[4,8],8]]]','[[[[0,5],3],[7,0]],9]','[[[[1,4],[4,3]],7],[[9,4],[6,[8,6]]]]','[[[7,2],[[3,3],1]],[5,9]]','[[[9,[6,2]],2],[[6,5],6]]','[[5,[3,2]],[[[2,4],[1,5]],[6,3]]]','[6,3]','[[9,6],[[[8,2],[5,6]],[[3,5],[3,3]]]]','[[[[2,5],7],4],[8,3]]','[[[[6,1],9],[0,6]],[6,2]]','[[[[8,4],2],[[0,1],[5,8]]],9]','[[[7,0],[4,9]],[[[9,9],[4,4]],[6,6]]]','[[[9,8],[2,0]],[[9,[6,2]],[6,[5,6]]]]','[[[9,8],[[0,6],[3,5]]],[[[4,7],[7,5]],[7,[8,5]]]]','[[[[9,0],[1,6]],[2,[5,3]]],[[[2,0],[0,3]],[[9,1],[7,7]]]]','[[[5,[2,2]],[2,[1,0]]],[1,1]]','[[[9,[7,2]],[[2,7],1]],[[5,7],[[8,7],7]]]','[[[9,[9,4]],[[0,8],2]],[0,[[2,2],[4,1]]]]','[[[5,5],[9,[2,0]]],[[[9,0],6],1]]','[[[1,9],[[9,5],[5,6]]],[6,[5,[9,4]]]]','[[[[8,6],9],9],[[7,2],[7,[2,6]]]]','[[[[6,4],7],7],[[2,[9,7]],7]]','[[7,[[5,6],9]],[[[9,8],8],[[8,9],[1,0]]]]','[[[0,[7,6]],0],[[[2,5],1],9]]','[[[3,[4,1]],[4,2]],[0,[[6,0],[1,6]]]]','[[9,[0,0]],[[[3,0],[9,9]],[1,[1,5]]]]','[[[[9,9],1],6],[5,6]]','[3,4]','[[[[5,4],9],6],2]','[[5,4],[[6,[7,4]],[[0,3],0]]]','[[[3,[9,6]],4],[[[9,8],6],3]]','[[5,[1,[5,5]]],[[[3,8],[0,1]],[[9,3],[6,2]]]]','[[4,[0,3]],1]','[[[7,[2,9]],[[5,8],2]],[[[4,4],[2,0]],8]]','[[[[4,0],0],8],7]','[[[[3,0],0],[[6,0],3]],[[[1,5],1],[3,[0,0]]]]','[[[[8,1],5],0],[[[3,9],[8,3]],[[6,9],[5,1]]]]','[[7,7],[[[8,5],2],[9,2]]]','[[[[4,9],9],[6,[5,3]]],[[[7,1],[7,1]],[[9,5],[7,0]]]]','[[7,[0,5]],[7,[2,[1,6]]]]','[[9,[0,[0,2]]],[[1,1],[[6,6],[5,3]]]]','[[[2,9],[[6,9],9]],[[[4,2],7],[1,[2,3]]]]','[[[0,1],[3,3]],[3,[[2,7],2]]]','[[[5,6],8],[[[4,9],[3,3]],[6,[5,2]]]]','[[4,[4,[2,5]]],[[2,[4,8]],[3,[7,7]]]]','[[2,5],[[[9,6],[9,3]],[[4,5],[2,3]]]]','[[5,[0,5]],[[[2,1],[0,5]],3]]','[[[[2,0],5],[[7,9],[4,5]]],[0,[[1,4],9]]]','[[[[1,3],2],[[3,9],[9,5]]],[[[4,1],[3,8]],0]]','[[[[1,8],[8,3]],[3,0]],[[5,1],[4,8]]]','[[1,6],[3,2]]','[[4,5],[[[9,3],[8,6]],[2,[8,6]]]]','[[[[4,4],1],[[7,3],2]],[[9,[2,1]],[8,2]]]','[0,[[2,[3,8]],9]]','[[1,[5,0]],[0,[[2,6],[8,5]]]]','[[6,[6,1]],[[2,[7,9]],[[8,3],1]]]','[[[2,[5,9]],[[8,9],1]],[[[5,2],2],4]]','[[[4,3],5],[[6,[3,6]],5]]','[1,[6,[6,2]]]','[[[[4,9],3],9],[[3,9],[8,[4,9]]]]','[[[[7,1],[1,6]],[[7,8],[3,7]]],[[[5,3],7],[9,[3,1]]]]','[[[[0,8],[8,9]],2],7]','[[[[3,7],[9,8]],[[7,1],8]],[[4,[4,6]],8]]','[3,[3,[[4,4],5]]]','[[3,[[2,3],7]],[[7,9],2]]','[[[[0,6],[5,1]],[[7,2],5]],[9,8]]','[[4,0],[[4,3],[7,2]]]','[[[8,[1,1]],[7,[9,1]]],[9,[9,[0,8]]]]','[9,[[[4,5],8],[[3,4],9]]]','[[[6,[4,7]],[8,7]],[[[3,8],5],[[2,1],[3,5]]]]','[[[[5,5],[6,8]],[[2,3],6]],[8,[5,7]]]','[[5,[[6,1],[3,6]]],[[[0,6],[7,1]],[9,[8,4]]]]','[[[[0,1],[4,9]],[[1,7],[3,3]]],[6,[3,[6,1]]]]','[[[[3,8],5],[[4,7],2]],2]','[[6,[[4,4],0]],[[2,[4,5]],[8,2]]]','[[6,[9,[7,0]]],[[9,[1,6]],[[6,1],1]]]','[[[[2,1],[5,7]],[5,[9,3]]],[[[7,9],[4,2]],4]]','[[3,1],[[7,8],[[8,8],9]]]','[[[[9,4],[1,8]],[9,[3,7]]],[[6,9],[[7,2],1]]]','[[[9,3],2],9]']
+
+
+# part 1
+
+def do_homework(data):
+	"""add all numbers, then get the magnitude of the final sum"""
+
+	index = 1
+	running_sum = data[0]
+	while index < len(data):
+		running_sum = add_snail_numbers(running_sum, data[index])
+		running_sum = ''.join([str(x) for x in running_sum])
+		index += 1
+
+	return get_magnitude(eval(running_sum))
+
+
+
+def add_snail_numbers(a, b):
+	"""adds 2 numbers using these stupid rules"""
+
+	total = '[' + a + ',' + b + ']'
+	return reduce_snail_number(list(total))
+
+
+
+import math
+def reduce_snail_number(old_list):
+	"""reduces the snail number using an even stupider rule"""
+
+	# convert string to list
+	index = 0
+	while index < len(old_list):
+		try:
+			old_list[index] = int(old_list[index])
+		except:
+			pass
+		index += 1
+
+	# check for explodes first
+	temp_list = []
+	
+	nest_level = 0
+	previous_regular_number_index = None
+	hold_for_next_regular_number = None
+	action_taken = False
+
+	index = 0
+	while index < len(old_list):
+		if old_list[index] == ' ':
+			index += 1
+			continue
+
+		if old_list[index] == ',':
+			temp_list.append(old_list[index])
+			index += 1
+			continue
+		
+		if old_list[index] == '[':
+			nest_level += 1
+
+			# pair is 4 levels in
+			if nest_level > 4 and not action_taken:
+				# get whole pair
+				pair_end = old_list.index(']', index)
+				pair = [old_list[index + 1], old_list[pair_end - 1]]
+				
+				# go back and add left value to previous regular number, if any
+				if previous_regular_number_index:
+					temp_list[previous_regular_number_index] += pair[0]
+
+				# hold the other number until the next number comes up
+				hold_for_next_regular_number = pair[1]
+
+				# don't do another explode at this time
+				action_taken = True
+
+				# replace whole pair with a 0
+				temp_list.append(0)
+				nest_level -= 1
+				index = pair_end + 1
+				continue
+
+			else:
+				temp_list.append(old_list[index])
+				index += 1
+				continue
+
+		if old_list[index] == ']':
+			nest_level -= 1
+			temp_list.append(old_list[index])
+			index += 1
+			continue
+
+		if type(old_list[index]) == int:
+			if action_taken and hold_for_next_regular_number:
+				# add the explode's right value to this number
+				temp_list.append(hold_for_next_regular_number + old_list[index])
+				previous_regular_number_index = None
+				hold_for_next_regular_number = None
+
+				index += 1
+				continue
+
+			else:
+				# remember this number in case of explode
+				if not action_taken:
+					previous_regular_number_index = len(temp_list)
+
+				temp_list.append(old_list[index])
+				index += 1
+				continue
+
+	# then check for splits
+	if not action_taken:
+		final_list = []
+
+		index = 0
+		while index < len(temp_list):
+			if temp_list[index] in [',', '[', ']']:
+				final_list.append(temp_list[index])
+				index += 1
+				continue
+
+			if type(temp_list[index]) == int:
+				# multi-digit?
+				if not action_taken and temp_list[index] > 9:
+					action_taken = True
+					final_list.append('[')
+					final_list.append(math.floor(temp_list[index] / 2))
+					final_list.append(',')
+					final_list.append(math.ceil(temp_list[index] / 2))
+					final_list.append(']')
+					index += 1
+					continue
+
+				# remember this number in case of explode
+				else:
+					final_list.append(temp_list[index])
+					index += 1
+					continue
+
+	else:
+		final_list = temp_list
+
+	# no change? done
+	if str(old_list) == str(final_list):
+		return final_list
+	else:
+		return reduce_snail_number(final_list)
+
+
+
+def get_magnitude(pair):
+	"""given string-like snail number, do stupid math to get magnitude"""
+
+	value = 0
+	if type(pair[0]) == int:
+		value += (3 * pair[0])
+	else:
+		value += (3 * get_magnitude(pair[0]))
+
+	if type(pair[1]) == int:
+		value += (2 * pair[1])
+	else:
+		value += (2 * get_magnitude(pair[1]))
+
+	return value
+
+
+
+# print(do_homework(data))
+
+
+
+# part 2
+
+def find_largest_magnitude(data):
+	"""given all data, find the magnitude of the sum of each possible pair, and return the highest possible magnitude"""
+
+	highest_possible_magnitude = 0
+	for x in data:
+		for y in data:
+			if x == y:
+				continue
+			this_magnitude = do_homework([x, y])
+			if this_magnitude > highest_possible_magnitude:
+				highest_possible_magnitude = this_magnitude
+
+	return highest_possible_magnitude
+
+
+
+# print(find_largest_magnitude(data))
